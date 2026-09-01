a3ea4a5165d52dc6c3bb1262a92521ad8c16ae86
jaskaranvirk01 <virk7184@gmail.com>
2026-09-01 17:10:51 +0530

    databse alembic initial migration done

--- src/blogforge_ai/database/session.py
+++ src/blogforge_ai/database/session.py
@@ -2,17 +2,33 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy.exc import SQLAlchemyError, ArgumentError
 from blogforge_ai.core.settings import settings
+from contextlib import contextmanager
+from collections.abc import Generator
 
 
 class DatabaseManager:
+    '''Manages the SQLAlchemy database engine and session factory'''
+
     def __init__(self):
         try:
             self.engine = create_engine(
                 settings.supabase_database_url, echo=True, pool_pre_ping=True)
             self.session_local = sessionmaker(
-                bind=self.engine, autoflush=False)
+                bind=self.engine, autoflush=False, expire_on_commit=False)
         except (SQLAlchemyError, ArgumentError) as e:
             raise e
 
     def get_session(self) -> Session:
         return self.session_local()
+
+    @contextmanager
+    def session(self) -> Generator[Session, None, None]:
+        db = self.get_session()
+        try:
+            yield db
+            db.commit()
+        except Exception:
+            db.rollback()
+            raise
+        finally:
+            db.close()
